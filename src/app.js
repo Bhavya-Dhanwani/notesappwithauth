7c7c2832c10dc2d2cd4dda56d7b6ffd25d061868
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-28 15:44:02 +0530

    Connected to the database

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import mainRouter from "./routes/mainRouter.route.js";
+import connectDB from "./config/db.config.js";
 
 // Configuring the env
 config();
@@ -13,6 +14,9 @@ const app = express();
 // adding Middlewares 
 app.use(express.json());
 app.use(cookieParser());
+
+// Connecting the DB
+await connectDB();
 
 // Configuring the Main router
 app.use("/api", mainRouter);
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+
+/*
+
+@Router signup 
+@Access Public
+@Use - To create new user
+
+*/
+function signup(req, res) {}
